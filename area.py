d7252a1e89ad8078c72d62ad3e509aa9b00544be
frickler01 <frickler01@users.noreply.github.com>
2016-05-06 23:32:12 +0200

    implement board features

--- area.py
+++ area.py
@@ -1,72 +1,102 @@
 # -*- coding: utf-8 -*-
-import random
+import random,copy
 from tabulate import tabulate
 
-WIDTH = 10
-HEIGHT = 5
-
 colors = ["red","green","blue","yellow","magenta"]
-
-def sign(x): return 1 if x >= 0 else -1
-
-def neg(x): return -1 if x < 0 else 0
 
 class Board():
 	def __init__(self,width,height):
 		self.width = width
 		self.height = height
 		self.area = [	[random.randint(0,len(colors)-1) for x in range(width)] \
-		             	for x in range(height)]
+		             							for x in range(height)]
 
 		# set up starting corners
-		self.set_corners()
-		self.set_edge()
+		self.set_start(0,0)
+		self.set_start(self.height-1,self.width-1)
 
-	def set_color(self,x,y,color):
+	def set_color_recursive(self,x,y,color):
+		# set color to area around given cell
 		ref = self.area[x][y]
 		self.area[x][y] = color
-		for a,b in self.neighbors(x,y):
+		for a,b in self.get_neighbors(x,y):
 			if self.area[a][b] == ref:
 				self.set_color(a,b,color)
 
+	def set_color(self,x,y,color):
+		a,_ = self.get_area(x,y)
+		for i,j in a:
+			self.area[i][j] = color
 
-	def neighbors(self,x,y):
+	def get_neighbors(self,x,y):
 		# return coordinates of neighbors
 		dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 		return [	[x+a,y+b] for a,b in dirs if \
 		        	all([x+a < self.height,y+b < self.width,x+a >= 0,y+b >= 0])]
 
-	def set_corners(self):
-		# set neighbors of starting point to same color
-		for x,y in self.diagonal(1):
-			self.area[x][y] = self.area[0][0]
-			self.area[-x-1][-y-1] = self.area[-1][-1]
+	def get_area(self,x,y):
+		# return coordinates of area of x,y and its border
+		todo = [[x,y]]
+		area = []
+		border = []
 
-	def set_edge(self):
-		# set neighbors of starting corner to different colors
-		for x,y in self.diagonal(2):
-			if self.area[x][y] == self.area[0][0]:
-				self.area[x][y] = (self.area[x][y] + 1) % (len(colors)-1)
-			if self.area[-x-1][-y-1] == self.area[-1][-1]:
-				self.area[-x-1][-y-1] = (self.area[-x-1][-y-1] + 1) % (len(colors)-1)
+		while todo != []:
+			a,b = todo[0]
+			area.append([a,b])
 
-	def diagonal(self,n):
-		# return coordinates of n-th diagonal
-		return [	[i+neg(n),n-i+neg(n)] \
-		        	for i in range(0,n+sign(n),sign(n)) \
-		        	if i+neg(n) < self.height and n-i+neg(n) < self.width]
+			border +=	filter( \
+			         	lambda k: k not in border and \
+			         	self.area[k[0]][k[1]] != self.area[x][y], \
+			         	self.get_neighbors(a,b))
+
+			todo +=	filter( \
+			       	lambda k: k not in area and \
+			       	self.area[k[0]][k[1]] == self.area[x][y], \
+			       	self.get_neighbors(a,b))
+			todo = todo[1:]
+
+		return area,border
+
+	def print_area(self,x,y):
+		# print area of given cell as `*`, border as `X`, `:` else
+		area = copy.deepcopy(self.area)
+		a,b = self.get_area(x,y)
+		for i in range(len(area)):
+			for j in range(len(area[0])):
+				if [i,j] in a:
+					area[i][j] = '*'
+				elif [i,j] in b:
+					area[i][j] = 'X'
+				else:
+					area[i][j] = ':'
+		print tabulate(area)
+
+	def set_start(self,x,y):
+		for a,b in self.get_neighbors(x,y):
+			# set immediate neighbors to same color
+			self.area[a][b] = self.area[x][y]
+			for c,d in filter(lambda k: k != [x,y],self.get_neighbors(a,b)):
+				# set 2nd-grade neighbors to different color
+				if self.area[c][d] == self.area[x][y]:
+					self.area[c][d] = (self.area[c][d] + 1) % (len(colors)-1)
 
 	def __str__(self):
 		return tabulate(self.area)
 
 def main():
+	HEIGHT = 5
+	WIDTH = 10
 
 	a = Board(WIDTH,HEIGHT)
-	a.set_color(0,0,a.area[0][0]+1 % 4)
-	a.set_color(0,0,a.area[0][0]+1 % 4)
-	a.set_color(0,0,a.area[0][0]+1 % 4)
-
 	print a
+	a.print_area(0,0)
+
+	for i in range(10):
+		print i
+		a.set_color(0,0,(a.area[0][0]+1) % 5)
+		print a
+		a.print_area(0,0)
+
 
 if __name__ == '__main__':
 	main()
